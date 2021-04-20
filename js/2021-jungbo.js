78b6f332cd8d92e3c7d1ca2037dcf8d795c977ce
All PGS 모든 프로그래머들 <kms0219kms@naver.com>
2021-04-20 18:51:25 +0900

    Create 2021-jungbo.js

---
+++ js/2021-jungbo.js
@@ -0,0 +1,5 @@
+        setTimeout(function() {
+          console.clear();
+          document.title = "2021 정보윤리 프로젝트";
+          console.log('%cThis Page is Successfully Loaded!', 'color:green; font-size:35px;');
+        }, 1500);
