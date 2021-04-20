30f968400f4c4b2800a8f5c988aa546d039fb64a
All PGS 모든 프로그래머들 <kms0219kms@naver.com>
2021-04-20 18:57:45 +0900

    Delete js directory

--- js/2021-jungbo.js
+++
@@ -1,5 +0,0 @@
-        setTimeout(function() {
-          console.clear();
-          document.title = "2021 정보윤리 프로젝트";
-          console.log('%cThis Page is Successfully Loaded!', 'color:green; font-size:35px;');
-        }, 1500);
